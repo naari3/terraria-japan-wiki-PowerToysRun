c8ee781c57b58d59f5936b745065089d238d6f09
naari3 <naari.named@gmail.com>
2022-05-29 12:17:16 +0900

    Delete unused file

--- src/naari3.TrJpWiki/pack.bat
+++
@@ -1,8 +0,0 @@
-dotnet build -c Release /p:TF_BUILD=true
-dotnet msbuild /t:ILMergeRelease
-
-xcopy /y .\bin\Release\net6.0-windows\Images\ .\bin\Release\netcoreapp3.1\Images\
-xcopy /y .\bin\Release\net6.0-windows\naari3.TrJpWiki.* .\bin\Release\netcoreapp3.1\
-xcopy /y .\bin\Release\net6.0-windows\plugin.json .\bin\Release\\netcoreapp3.1\
-
-:: Zip '.\bin\Release\\netcoreapp3.1\' into '\netcoreapp3.1.PowerToysRun.1.0.0.zip'
